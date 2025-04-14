98db1fa93ed6892ee66e3d718961702f079ee7bb
VictorioLaptop <v.garciadelapinera@lyon.ort.asso.fr>
2025-04-15 01:02:48 +0200

    [Test] GitTest From Fedora -- VictorioGarcia

--- RiskOfRain.Web/Config/ServicesExtensionMethods.cs
+++ RiskOfRain.Web/Config/ServicesExtensionMethods.cs
@@ -21,6 +21,7 @@ public static class ServicesExtensionMethods
         {
             options.UseSqlServer(connectionString);
         });
+         
         
         
         services.AddIdentityCore<User>()
